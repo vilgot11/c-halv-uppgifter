17431f9c1546f002b071770fc004a1e71e5f80ee
vilgot11 <vilgot.ahlfors@gmail.com>
2024-12-04 10:57:05 +0200

    Add files via upload

---
+++ C#Uppgift18/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Uppgift18
+{
+    internal class Program
+    {
+
+
+        
+        static void Main(string[] args)
+        {
+            myMethod();
+
+
+
+
+
+        }
+        static void myMethod()
+        {
+            Console.WriteLine("Enter Widght: ");
+            int Widght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Height: ");
+            int Height = Convert.ToInt32(Console.ReadLine());
+
+            int area = (Widght * Height) / 2;
+
+            Console.WriteLine($"The area of the triangle is {area}");
+        }
+
+    }
+        
+        
+    }
+
